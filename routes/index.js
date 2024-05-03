10906f30de177407977242e7231ae4cab0ba1409
alexandrea parks <alparks@idk.com>
2024-05-02 22:38:08 -0500

    bmi app completed

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,43 @@
 // create a function to create a webpage in response to different URLs
 // each URL has its own function that creates the webpage sent to the browser
 
-const express = require('express')
-const {request} = require("express");
-const router = express.Router()
+const express = require('express')  // import express library
+const router = express.Router()  // this router object does the routing - based on URL and method
 
-router.get('/', function (req, res) {
+// GET request to home page
+router.get('/', function (req, res, next) {
     console.log(req.query)
-    const height = req.query.height
-    res.render('index.hbs')
+    // render template and optional data together
+    res.render('index.hbs', {
+        title: 'Body Mass Index Calculator',
+        author: 'Alexandrea'
+    })
+})
+
+// GET request for calculating BMI - used when form is submitted (calculated button is clicked)
+// renders to a bmi results page
+router.get('/calculate', function(req, res, next) {
+    // get height and weight data from URL query
+    const bmiFormData = req.query  // get URL query
+    const height = bmiFormData.height
+    const weight = bmiFormData.weight
+    console.log(bmiFormData)
+
+    if (weight === '' || height === '' || weight <= 0 || height <= 0) {
+        // unsure of how to show an alert to enter valid height and weight
+        // tried res.send() but it directs to a whole new page with the message
+        // res.send('Please enter a valid height and weight')
+        return
+    }
+
+    // calculate bmi and format with 2 decimal places
+    const bmi = (weight / (height * height)).toFixed(2)
+    console.log(bmi)
+
+    // render bmi results template with BMI result data
+    res.render('results', {
+        bmi: bmi
+    })
 })
 
 
--- server.js
+++ server.js
@@ -3,23 +3,31 @@
 // web server creates web pages in response to a request from a browser
 // then the browser can display that page to a user
 
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require('path')  // import path library
+const express = require('express')  // import express library
+const bodyParser = require('body-parser')  // imported this in class, but don't think it's necessary? no POST request
 
 // routing is figuring out what page user wants - usually is based on URL
-const indexRouter = require('./routes/index.js')
+const indexRouter = require('./routes/index.js')  // import routes
 
 const app = express()
 
 // enable parsing of POST request form body
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// server static files
+const staticFileLocation = path.join(__dirname, 'public')  // where static files are stored
+app.use(express.static(staticFileLocation))  // tell application to use static files and where they can be found
+
+
 // configure it to use the Handlebars template engine and
 // work with template files in the views directory
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
-app.use('/', indexRouter)
+app.use('/', indexRouter)  // make routes in index.js available to application
 
-let server = app.listen(3000)
+// start server
+let server = app.listen(process.env.PORT || 3000, function () {  // use specific port OR 3000
+    console.log('Server is running on port ' + server.address().port)
+})
